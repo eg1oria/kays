c5029320d87f949518f732ff8ff9161586bcd178
Eglor <leontevegor57@gmail.com>
2026-05-21 11:47:25 +0500

    -

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/kays8",
+        destination: "/kays8.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
